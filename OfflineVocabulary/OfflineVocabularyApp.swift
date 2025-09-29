e64bab65185b819f347e064c435e55a65d1d3e5b
phu.phung80 <phu.phung80@gmail.com>
2025-09-29 15:32:41 -0500

    Initial Commit

---
+++ OfflineVocabulary/OfflineVocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineVocabularyApp.swift
+//  OfflineVocabulary
+//
+//  Created by Phung Phu on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct OfflineVocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OfflineVocabularyTests/OfflineVocabularyTests.swift
@@ -0,0 +1,16 @@
+//
+//  OfflineVocabularyTests.swift
+//  OfflineVocabularyTests
+//
+//  Created by Phung Phu on 9/29/25.
+//
+
+import Testing
+
+struct OfflineVocabularyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
